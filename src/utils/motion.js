7bd684fbcc0b2eba6698e738efac7747b29eecd6
Franco <francogomez23800@gmail.com>
2023-09-18 16:01:00 -0300

    profile picture

--- src/utils/motion.js
+++ src/utils/motion.js
@@ -99,19 +99,35 @@ export const textVariant = (delay) => {
   }
 
  export const titleVariants = {
-    hidden: { opacity: 0, y: 20 },
+  hidden: {
+    y: 20,
+    opacity: 0,
+  },
     visible: {
-      opacity: 1,
       y: 0,
-      transition: { duration: 1, delay: 0.5 },
+      opacity: 1,
+      transition: {
+        type: "spring",
+        duration: 0.8,
+        damping: 20,
+        delay: 0.5,
+      },
     },
   };
 
  export const subtitleVariants = {
-    hidden: { opacity: 0, y: 20 },
+  hidden: {
+    y: 20,
+    opacity: 0,
+  },
     visible: {
-      opacity: 1,
       y: 0,
-      transition: { duration: 1, delay: 1 },
+      opacity: 1,
+      transition: {
+        type: "spring",
+        duration: 1,
+        damping: 20,
+        delay: 1,
+      },
     },
   };
